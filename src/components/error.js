7e625656a9226fa45fe107f50ced2654d3a9a2a5
Kinwaa Leung <andy19feb@gmail.com>
2019-05-12 22:38:58 +0800

    myapp

---
+++ src/components/error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <p>Error: Path dose not exists</p>
+        </div>
+    );
+};
+
+export default Error;
